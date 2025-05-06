b6de7d45bf9f5ae72b106b0510efbe27b2186611
Semino <abati.matteo@itismeucci.com>
2025-05-06 11:23:55 +0200

    progettoFinito

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Corridore c1 = new Corridore("marco");
+        Corridore c2 = new Corridore("saul");
+        Corridore c3 = new Corridore("cristian");
+        Corridore c4 = new Corridore("giovanni");
+
+        c1.start();
+        c2.start();
+        c3.start();
+        c4.start();
     }
 }
